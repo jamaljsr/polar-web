d648a2c33959d66ebfaac902ac47802275ebfa45
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2022-08-31 04:22:19 -0400

    update version to v1.4.0

--- js/custom.js
+++ js/custom.js
@@ -32,7 +32,7 @@ $(window).on('scroll', function () {
 /*----------------------------------------------------*/
 /*	Download links
 	/*----------------------------------------------------*/
-var version = '1.3.0';
+var version = '1.4.0';
 
 function getOS() {
   var userAgent = window.navigator.userAgent,
